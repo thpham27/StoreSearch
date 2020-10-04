48d7e0e30ff6ca456d182d3206eac1717a44e95a
Pham <tpham27@live.spcollege.edu>
2020-10-04 16:55:48 -0400

    Add a search bar and table view. The search puts fake results in the table for now

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Thanh Pham on 10/4/20.
+//  Copyright © 2020 Thanh Pham. All rights reserved.
+//
+
+import Foundation
+    class SearchResult {
+      var name = ""
+      var artistName = ""
+    }
