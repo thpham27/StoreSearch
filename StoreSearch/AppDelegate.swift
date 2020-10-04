0b98ae1f4d16a4975afb2b7dc93455cddf9bbcd7
Pham <tpham27@live.spcollege.edu>
2020-10-04 18:20:36 -0400

    page 1082

--- StoreSearch/AppDelegate.swift
+++ StoreSearch/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+           customizeAppearance()
         return true
     }
 
@@ -32,6 +32,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
+    // MARK:- Helper Methods
+    func customizeAppearance() {
+      let barTintColor = UIColor(red: 20/255, green: 160/255,
+    blue: 160/255, alpha: 1)
+        UISearchBar.appearance().barTintColor = barTintColor
+    }
 }
 
--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -6,6 +6,27 @@
 //  Copyright © 2020 Thanh Pham. All rights reserved.
 //
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import UIKit
 
 class SearchViewController: UIViewController {
@@ -15,11 +36,23 @@ class SearchViewController: UIViewController {
     var searchResults = [SearchResult]()
     var hasSearched = false
     
+    struct TableView {
+    struct CellIdentifiers {
+      static let searchResultCell = "SearchResultCell"
+        static let nothingFoundCell = "NothingFoundCell"
+        
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
          tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
-        // Do any additional setup after loading the view.
-    }
+        var cellNib = UINib(nibName:TableView.CellIdentifiers.searchResultCell, bundle: nil)
+        tableView.register(cellNib, forCellReuseIdentifier: TableView.CellIdentifiers.searchResultCell)
+        cellNib = UINib(nibName:
+            TableView.CellIdentifiers.nothingFoundCell, bundle: nil)
+        tableView.register(cellNib, forCellReuseIdentifier: TableView.CellIdentifiers.nothingFoundCell)
+        searchBar.becomeFirstResponder()    }
 
 
 }
@@ -64,31 +97,24 @@ class SearchViewController: UIViewController {
         
         func tableView(_ tableView: UITableView,
         willSelectRowAt indexPath: IndexPath) -> IndexPath? {
-        if searchResults.count == 0 { return nil
+        if searchResults.count == 0 {
+            return nil
         } else {
             return indexPath
           }
         }
         
-     func tableView(_ tableView: UITableView,
-                    cellForRowAt indexPath: IndexPath) ->
-        UITableViewCell {
-     let cellIdentifier = "SearchResultCell"
-     var cell:UITableViewCell! = tableView.dequeueReusableCell(
-                                   withIdentifier: cellIdentifier)
-     if cell == nil {
-     cell = UITableViewCell(style: .subtitle,
-                            reuseIdentifier: cellIdentifier)
-        }
-     if searchResults.count == 0 {
-        cell.textLabel!.text = "(Nothing found)"
-        cell.detailTextLabel!.text = ""
-     } else {
-     let searchResult = searchResults[indexPath.row]
-        cell.textLabel!.text = searchResult.name
-        cell.detailTextLabel!.text = searchResult.artistName
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       if searchResults.count == 0 {
+         return tableView.dequeueReusableCell(withIdentifier:
+           TableView.CellIdentifiers.nothingFoundCell, for: indexPath)
+       } else {
+         let cell = tableView.dequeueReusableCell(withIdentifier: TableView.CellIdentifiers.searchResultCell, for: indexPath) as! SearchResultCell
+         let searchResult = searchResults[indexPath.row]
+         cell.nameLabel.text = searchResult.name
+         cell.artistNameLabel.text = searchResult.artistName
+         return cell
+       }
      }
-        return cell
-}
 }
 
