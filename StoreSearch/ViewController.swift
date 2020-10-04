eb4cd5897e18ce4e87f0008d7ae8404667bc65b6
Pham <tpham27@live.spcollege.edu>
2020-10-04 15:35:58 -0400

    Initial Commit

---
+++ StoreSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by Thanh Pham on 10/4/20.
+//  Copyright © 2020 Thanh Pham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
