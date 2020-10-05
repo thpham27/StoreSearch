a2b873d903acaebb58605cb9f1d18ffc6a7732dc
Pham <tpham27@live.spcollege.edu>
2020-10-05 17:00:24 -0400

    "Add fetching data from web service using synchronous network request".

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -7,31 +7,12 @@
 //
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import UIKit
 
 class SearchViewController: UIViewController {
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
+    
     
     var searchResults = [SearchResult]()
     var hasSearched = false
@@ -57,28 +38,27 @@ class SearchViewController: UIViewController {
 
 }
     extension SearchViewController: UISearchBarDelegate {
+        
         func position(for bar: UIBarPositioning) -> UIBarPosition {
         return .topAttached
             
         }
         
         func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-            
+            if !searchBar.text!.isEmpty {
         searchBar.resignFirstResponder()
-        searchResults = []
-        if searchBar.text! != "justin bieber" {
-            for i in 0...2 {
-        let searchResult = SearchResult()
-            searchResult.name = String(format: "Fake Result %d for", i)
-            searchResult.artistName = searchBar.text!
-            searchResults.append(searchResult)
-    }
-            }
             hasSearched = true
-            tableView.reloadData()
+            searchResults = []
+        let url = iTunesURL(searchText: searchBar.text!)
+            print("URL: '\(url)'")
+                
+                if let data = performStoreRequest(with: url) {
+                   searchResults = parse(data: data)
+                     searchResults.sort { $0 < $1 } // alphabet order
             }
+        tableView.reloadData() }
         }
-
+}
     extension SearchViewController: UITableViewDelegate,
                                      UITableViewDataSource {
         
@@ -112,9 +92,51 @@ class SearchViewController: UIViewController {
          let cell = tableView.dequeueReusableCell(withIdentifier: TableView.CellIdentifiers.searchResultCell, for: indexPath) as! SearchResultCell
          let searchResult = searchResults[indexPath.row]
          cell.nameLabel.text = searchResult.name
+        
          cell.artistNameLabel.text = searchResult.artistName
          return cell
        }
      }
+        
+        // MARK:- Helper Methods
+        func iTunesURL(searchText: String) -> URL {
+            let encodedText = searchText.addingPercentEncoding(
+                withAllowedCharacters: CharacterSet.urlQueryAllowed)!
+            let urlString = String(format:
+                "https://itunes.apple.com/search?term=%@", encodedText)
+            let url = URL(string: urlString)
+        return url!
+        }
+        
+        func performStoreRequest(with url: URL) -> Data? {
+            do {
+                return try Data(contentsOf:url)
+            }
+            catch {
+        print("Download Error: \(error.localizedDescription)")
+                   showNetworkError()
+        return nil
+        } }
+        
+        func parse(data: Data) -> [SearchResult] {
+            do {
+        let decoder = JSONDecoder()
+        let result = try decoder.decode(ResultArray.self, from:data)
+                return result.results
+          } catch {
+            print("JSON Error: \(error)")
+        return [] }
+        }
+        
+        func showNetworkError() {
+        let alert = UIAlertController(title: "Whoops...",
+        message: "There was an error accessing the iTunes Store." + " Please try again.", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default,
+                                 handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        }
 }
+
+
 
